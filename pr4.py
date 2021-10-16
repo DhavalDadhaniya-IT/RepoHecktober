789e8d554137cf1bb00bd3b1eb3c99c89ed7661e
arshad32365 <72139162+arshad32365@users.noreply.github.com>
2021-10-16 11:26:19 +0500

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -9,3 +9,14 @@ if m in range(5,9):
     
 if m in range(9,13):
     print("Monsoon")
+    
+    # Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
